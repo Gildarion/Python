ff50b4a59c67a73e9adb8a4c77c3403db8812323
gildarion <jmarcab01@gmail.com>
2019-04-10 20:39:04 +0200

    Classic functions

---
+++ module2/function/ps20.py
@@ -0,0 +1,12 @@
+def sumAllN(limitTo):
+    result = 0
+    for i in range(limit + 1):
+        result += i
+    return result
+
+def sumAllSquares(limitTo):
+    result = 0
+    for i in range(limitTo + 1):
+        result += i * i
+    return result
+
